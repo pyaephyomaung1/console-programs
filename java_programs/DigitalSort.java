7ffe3fd251e217e01675d8a6aeaa2419d4ff1183
pyaephyo <pyaephyomg.ppm06@gmail.com>
2025-04-10 01:04:43 +0700

    Sorting

---
+++ java_programs/DigitalSort.java
@@ -0,0 +1,22 @@
+package java_programs;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DigitalSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        String input = scanner.nextLine();
+        char[] digits = input.toCharArray();
+
+        Arrays.sort(digits);
+        System.out.println("Sorted digits: ");
+        for (char digit: digits) {
+            System.out.print(digit + " ");
+        } 
+        System.out.println();
+        System.out.println("The sorted digits are: " + new String(digits));
+        scanner.close();
+    }
+}
