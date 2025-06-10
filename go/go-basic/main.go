dcd74f8c91c77768353ced2a699d4084441dd22c
Pyae Phyo Maung <pyaephyomg.ppm06@gmail.com>
2025-06-11 01:12:02 +0700

    Go Learning

--- go/go-basic/main.go
+++ go/go-basic/main.go
@@ -1,7 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	ch := make(chan int, 0)
-	<-ch
-	close(ch)
+	ch := make(chan string)
+	quit := make(chan bool)
+
+	go func() {
+		for i := 0; i < 3; i++ {
+			ch <- fmt.Sprintf("Gopher 1 say hi %d", i)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 3; i++ {
+			ch <- fmt.Sprintf("Gopher 2 say hi %d", i)
+			time.Sleep(700 * time.Millisecond)
+		}
+		quit <- true
+	}()
+
+	for {
+		select {
+		case msg := <-ch:
+			fmt.Println("Message", msg)
+		case <-quit:
+			fmt.Println("Quitting")
+			return
+		}
+	}
 }
